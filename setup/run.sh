a56919cbb6d0c3f3db69c757ddf6037a09c7c22c
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-01-28 22:23:09 +0000

    TF2.0

--- setup/run.sh
+++ setup/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./config
-docker run -d -p $SSH_PORT:22 --rm -it --shm-size=$SHARED_MEMORY -v /mnt/data:/mnt/data -v /mnt/share:/mnt/share -v /home/:/mnt/home --gpus all -it --name $CONTAINER_NAME $IMAGE_NAME:latest /bin/bash -c '/usr/sbin/sshd -D&& tail -f /dev/null'
+docker run -d -p $SSH_PORT:22 --rm --shm-size=$SHARED_MEMORY -v /mnt/data:/mnt/data -v /mnt/share:/mnt/share -v /home/:/mnt/home --gpus all -it --name $CONTAINER_NAME $IMAGE_NAME:latest /bin/bash -c '/usr/sbin/sshd -D&& tail -f /dev/null'
